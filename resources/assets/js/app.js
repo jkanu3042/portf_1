9d0c55eb5483cd821355c5c834f2a3a17b948b43
jkanu3042 <jkanu3042@naver.com>
2017-09-09 19:08:56 +0900

    project_create

---
+++ resources/assets/js/app.js
@@ -0,0 +1,51 @@
+require('./bootstrap');
+window.Vue = require('vue');
+
+// Vue.component('example', require('./components/Example.vue'));
+
+const app = new Vue({
+    el: '#app',
+
+    ready() {
+    hljs.initHighlightingOnLoad();
+    this.removeFlashMessages();
+    this.setJqueryAjaxHeaders();
+    this.initBackToTopButton();
+},
+
+methods: {
+    removeFlashMessages() {
+        if ($('.alert')) {
+            $('.alert').delay(5000).fadeOut();
+        }
+    },
+
+    setJqueryAjaxHeaders() {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    },
+
+    initBackToTopButton() {
+        $('#back-to-top').on('click', function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+
+            return false;
+        });
+
+        $(window).on('scroll', function () {
+            var scrollPos = $(window).scrollTop();
+
+            if (scrollPos > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+    }
+}
+});
